367de94dcf6bfef67be1d664eb6a04686697d5d3
Prathamesh Jakkula <mankarmansvi@gmail.com>
2025-06-04 15:34:22 -1200

    first commit

---
+++ hello.js
@@ -0,0 +1,23 @@
+let val=19;
+val="hello";
+val=false;
+console.log(val);
+let n=3;
+console.log(n);
+if (n%2==0)
+    {
+
+   console.log("number is even");
+}
+   else
+   {
+   console.log("number is odd");
+}
+let name='alice';
+let greetings=`hello,
+${Name}!`;
+console.log(greetings);
+
+
+let x=10+5;
+console.log(y)
